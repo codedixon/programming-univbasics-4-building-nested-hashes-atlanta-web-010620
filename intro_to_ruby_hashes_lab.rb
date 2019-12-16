001a8ffbcb94f1714a88053ebfa687936953d51b
Rashad Dixon <dixoncodes@gmail.com>
2019-12-16 01:41:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
   new_hash = {:railroads => {
     pieces: 4,
     rent_in_dollars: {
-      names: {},
+      key1: {},
       key2: {},
       key3: {},
       key4: {}
